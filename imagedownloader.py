63e659252586f570ee250c67983784b876681a5a
Tyler <tsm33@hw.ac.uk>
2017-07-04 23:11:26 +0100

    Now works on linux! Python 3 still required.

--- imagedownloader.py
+++ imagedownloader.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 import os
 import sys
 import errno
+import platform
 boardList = ["a", "b", "h", "v", "vr", "vg", "gif", "g", "co", "e", "u", "w", "wg"]
-path = "C:/Users/Tyler -_-/Documents/4chanImages/"
-os.makedirs(os.path.dirname(path), exist_ok=True)
+operatingSystem = platform.system()
+imagePath = "C:/Users/Tyler -_-/Documents/4chanImages/"
+if operatingSystem == "Linux":
+	imagePath = "/home/ty/Pictures/4chanImages/"
 
 def download(url, file_name, percent, total):
 	completion = percent/total * 100
@@ -13,10 +16,9 @@ def download(url, file_name, percent, total):
 		completion = 100
 	#print(str(int(completion)) + "%" + " done")
 	sys.stdout.write("\r%i" % int(completion) + " %")
-	with open(path + file_name, 'wb') as file:
+	with open(imagePath + file_name, 'wb') as file:
 		response = requests.get(url)
 		file.write(response.content)
-
 def getImages(linkedThread, board):
 	soup = BeautifulSoup(linkedThread.content, 'html.parser')
 	threadHeader = soup.find('body', class_='is_thread')
@@ -25,11 +27,15 @@ def getImages(linkedThread, board):
 		download("http:"+imageThumbs[x]['href'], imageThumbs[x]['href'].split(board,1)[1], x, len(imageThumbs))
 
 def chooseThread():
+	global imagePath
 	board = input('Enter board: ')
 	board = board.replace('/', '')
 	while board not in boardList:
 		board = input('Not a valid board: ')
 		board = board.replace('/', '')
+	imagePath = imagePath + board + "/"
+	os.makedirs(os.path.dirname(imagePath), exist_ok=True)
+	print(imagePath)
 	postNum = input('Enter OP number: ')
 	createdThread = "http://boards.4chan.org/" + board + "/thread/" + postNum
 	request = requests.get(createdThread)
